6fc21ed2d25d7dce68619313f4b92626c7ecbfbd
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-02 15:03:35 +0200

    Feat: BackEnd from Local Repo

---
+++ Models/TodoItems.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+namespace TodoApi.Models;
+
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
